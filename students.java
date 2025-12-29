1599d25b2b7ab9bf6fcb2c9e6aca045554caf722
linagiri <np03cs4a240320@heraldcollege.edu.np>
2025-12-29 14:50:13 +0545

    Added a method called students

--- students.java
+++ students.java
@@ -1,5 +1,19 @@
 package week7workshop;
 
 public class students {
+    private String name;
+    private int age;
 
+    public students(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
